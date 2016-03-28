02db1318236dcc2a0ebe141a656508b8c02eccea
giftofjehovah <jonathan.pwh@gmail.com>
2016-03-28 12:15:50 +0800

    add actor controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,34 @@
 class ActorsController < ApplicationController
+	before_action :set_actor, only: [:show, :edit, :update, :destroy]
+
+	def show
+
+	end
+
+	def new
+		@actor = Actor.new
+	end
+
+	def create
+		@actor = Actor.new(actor_params)
+	end
+
+	def edit
+
+	end
+
+	def update
+		@actor.update(actor_params)
+	end
+
+	private
+
+	def set_actor
+		@actor = Actor.find(params[:id])
+	end
+
+	def actor_params
+		params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+	end
 
 end

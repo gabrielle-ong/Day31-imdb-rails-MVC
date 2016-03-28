14368e32427d1827e27d620aff2a2ac237846174
gabrielleong <gabrielleong97@gmail.com>
2016-03-28 12:24:42 +0800

    student 1 controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,35 @@
 class MoviesController < ApplicationController
 
+    def index
+        @movies = Movie.all
+    end
+    
+    def show
+    end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        @movie = Movie.create(movie_params)
+    end
+    
+    def edit
+    end
+    
+    def update
+        @movie.update(movie_params)
+    end
+    
+    Private
+    
+    def set_movie
+        @movie = Movie.find(params[:id])
+    end
+    
+    def movie_params
+        params.require(:movie).permit(:title, :summary, :youtube_embeded_id, :thumbnail)
+    end
+    
 end
